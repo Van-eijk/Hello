f11bcd4dcca8404c9b6bcf10815df871409ea228
van <siginingvan@yahoo.com>
2026-06-18 06:56:54 +0100

    Ajout de la page de la listes des conversations

---
+++ header.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-md nav-responsive navbar-dark fixed-top">
+    <div class="container main-nav">
+        <a href="hello-home.php" class="navbar-brand text-uppercase fw-bold text-dark">
+            <span class="bg-danger bg-gradient p-1 m-1 rounded-3 text-light">Hello </span>
+        </a>
+
+        <button class="navbar-toggler btn-danger" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"><i class="fa fa-navicon" style="color:#fff; font-size:28px;"></i></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav ">
+                <li class="nav-item ps-3">
+                    <a href="accueil.php" class="nav-link text-white active">Accueil</a>
+                </li>
+
+                <li class="nav-item ps-3">
+                    <a href="ajouterclient.php" class="nav-link text-white">Groupes</a>
+                </li>
+
+                <li class="nav-item ps-3">
+                    <a href="listeclient.php" class="nav-link text-white">Membres</a>
+                </li>
+
+                
+
+                <li class="nav-item ps-3">
+                    <a href="profilmembre.php" class="nav-link text-white"> User</a>
+                </li>
+
+                <li class="nav-item ps-3">
+                     <!-- Confert popupdeconnexion.php -->
+
+                    <a class="nav-link text-white"  data-bs-toggle="modal" data-bs-target="#staticBackdrop" style=" cursor: pointer;">Deconnexion</a>
+                </li>
+            </ul>
+
+        </div>
+
+
+    </div>
+</nav>
---
+++ hello-home.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello-home</title>
+
+    <!-- lien pour integrer le framework boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <script src="https://kit.fontawesome.com/14273d579a.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/hello-home-style.css">
+</head>
+<body>
+    <div class="container">
+        <?php include "header.php" ; ?>
+        <?php include 'popupdeconnexion.php'; //Confirmation de déconnexion de l'utilisateur ?>
+
+        <div class="main-content">
+
+            <a href="#">
+                <div class="item-message">
+                    <div class="icon-message">
+                        <span>
+                            <i class="bi bi-person-circle"></i>
+                        </span>
+
+                    </div>
+                    <div class="main-message border-bottom">
+                        <div class="sender-message">
+                            <p>Bobo</p>
+
+                        </div>
+                        <div class="message-text">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate officia placeat rerum deleniti accusamus voluptatibus vero ad, ducimus nesciunt vitae eaque praesentium ut quo mollitia, voluptatum explicabo temporibus in minima.</p>
+                        </div>
+
+                    </div>
+                    <div class="hour-message">
+                        <p>13:00</p>
+                        <div class="new-message">
+                            <span>1</span>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+
+
+            <a href="#">
+                <div class="item-message">
+                    <div class="icon-message">
+                        <span>
+                            <i class="bi bi-person-circle"></i>
+                        </span>
+
+                    </div>
+                    <div class="main-message border-bottom">
+                        <div class="sender-message">
+                            <p>Bobo</p>
+
+                        </div>
+                        <div class="message-text">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate officia placeat rerum deleniti accusamus voluptatibus vero ad, ducimus nesciunt vitae eaque praesentium ut quo mollitia, voluptatum explicabo temporibus in minima.</p>
+                        </div>
+
+                    </div>
+                    <div class="hour-message">
+                        <p>13:00</p>
+                        <div class="new-message">
+                            <span>1</span>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+
+
+
+
+            <a href="#">
+                <div class="item-message">
+                    <div class="icon-message">
+                        <span>
+                            <i class="bi bi-person-circle"></i>
+                        </span>
+
+                    </div>
+                    <div class="main-message border-bottom">
+                        <div class="sender-message">
+                            <p>Bobo</p>
+
+                        </div>
+                        <div class="message-text">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate officia placeat rerum deleniti accusamus voluptatibus vero ad, ducimus nesciunt vitae eaque praesentium ut quo mollitia, voluptatum explicabo temporibus in minima.</p>
+                        </div>
+
+                    </div>
+                    <div class="hour-message">
+                        <p>13:00</p>
+                        <div class="new-message">
+                            <span>1</span>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+
+
+
+
+            <a href="#">
+                <div class="item-message">
+                    <div class="icon-message">
+                        <span>
+                            <i class="bi bi-person-circle"></i>
+                        </span>
+
+                    </div>
+                    <div class="main-message border-bottom">
+                        <div class="sender-message">
+                            <p>Bobo</p>
+
+                        </div>
+                        <div class="message-text">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate officia placeat rerum deleniti accusamus voluptatibus vero ad, ducimus nesciunt vitae eaque praesentium ut quo mollitia, voluptatum explicabo temporibus in minima.</p>
+                        </div>
+
+                    </div>
+                    <div class="hour-message">
+                        <p>13:00</p>
+                        <div class="new-message">
+                            <span>1</span>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+</body>
+</html>
